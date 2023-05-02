116a5bd6dccd56e11d7dde728e315423c1cae331
James P <93916271+jaminthecloud@users.noreply.github.com>
2023-05-02 19:22:29 +0100

    jokes on you, this isnt even my final push

---
+++ Assets/Scripts/FloatingText.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour
+{
+    public Transform mainCam;
+
+    public Transform worldSpaceCanvas;
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main.transform;
+        transform.SetParent(worldSpaceCanvas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCam.transform.position); // look at camera
+
+        transform.position = target.position + offset;
+    }
+}
